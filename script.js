f576d8eeb3f188e210d3c0e989188a898a2b2d81
LestterX <lgs.alves@hotmail.com>
2024-01-19 15:47:46 -0300

    feat: firs commit. Everything working

---
+++ script.js
@@ -0,0 +1,24 @@
+const userInput = document.querySelector('.userInput');
+const bntSpeak = document.querySelector('.bntSpeak');
+const synth = window.speechSynthesis;
+
+const speak = () => {
+    let userInputMsg = userInput.value
+    let voices = synth.getVoices();
+    let msg = new SpeechSynthesisUtterance()
+    msg.rate = 2;
+    msg.pitch = 10;
+    msg.text = userInputMsg
+    if (String(userInputMsg).length < 1) msg.text = 'Por favor, digite a mensagem!'
+    msg.lang = 'pt-BR'
+    if (voices.length > 1) {
+        msg.voice = voices[1]
+        synth.speak(msg)
+    } else {
+        msg.voice = voices[0]
+        synth.speak(msg)
+    }
+}
+
+bntSpeak.addEventListener('click', () => { speak() });
+userInput.addEventListener('keypress', (e) => { if (e.key === 'Enter') { speak() } });
